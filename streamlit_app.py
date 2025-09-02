664b662dd87a4ba0b716cf4efb0453acd4e81326
ztaylor94 <96702610+ztaylor94@users.noreply.github.com>
2025-09-02 16:35:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 .# Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
 
-import openai
+from openai import OpenAI
 import streamlit as st
 
 with st.sidebar:
@@ -26,7 +26,7 @@ if prompt := st.chat_input():
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-    client = openai.OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
